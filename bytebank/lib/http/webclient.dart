4cc4b5a460dbbacf5e01eef235ef4a9071b1be34
Lucas Albertoni Santos <lucas.albertoni.santos@gmail.com>
2020-09-07 15:18:11 -0300

    Especializando exceções conforme acontecimentos. Signed-off-by: Lucas Albertoni Santos <lucas.albertoni.santos@gmail.com>

--- bytebank/lib/http/webclient.dart
+++ bytebank/lib/http/webclient.dart
@@ -4,6 +4,7 @@ import 'package:http_interceptor/http_interceptor.dart';
 
 final Client client = HttpClientWithInterceptor.build(
   interceptors: [LoggingInterceptor()],
+  requestTimeout: Duration(seconds: 5),
 );
 
 const String baseUrl = 'http://192.168.99.1:8080/transactions';
--- bytebank/lib/http/webclients/transaction_web_client.dart
+++ bytebank/lib/http/webclients/transaction_web_client.dart
@@ -8,7 +8,7 @@ import '../webclient.dart';
 class TransactionWebClient {
   Future<List<Transaction>> findAll() async {
     final Response response =
-        await client.get(baseUrl).timeout(Duration(seconds: 5));
+        await client.get(baseUrl);
     final List<dynamic> decodedJson = jsonDecode(response.body);
     return decodedJson
         .map((dynamic json) => Transaction.fromJson(json))
@@ -28,14 +28,17 @@ class TransactionWebClient {
     if (response.statusCode == 200) {
       return Transaction.fromJson(jsonDecode(response.body));
     }
-    _throwHttpError(response.statusCode);
+    throw HttpException(_statusCodeResponses[response.statusCode]);
   }
-
-  void _throwHttpError(int statusCode) =>
-      throw Exception(_statusCodeResponses[statusCode]);
 
   static final Map<int, String> _statusCodeResponses = {
     400: 'there was an error submitting transaction',
     401: 'authentication failed',
   };
+}
+
+class HttpException implements Exception {
+  final String message;
+
+  HttpException(this.message);
 }
